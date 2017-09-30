f9c34fad412b103d45f24fc93bf47e271062fe14
Nicholas Souphandavong <flamestream@gmail.com>
2017-09-30 18:49:18 -0400

    Remove ILMerge references.

--- App/merge-bins.bat
+++ App/merge-bins.bat
@@ -4,40 +4,43 @@ setlocal EnableDelayedExpansion
 set ilmerge_bin=..\..\..\packages\ILMerge.2.14.1208\tools\ILMerge.exe
 set out_dir=%1
 set out_file=%2
-set temp_file=temp.exe
-set exclusion=\%2.config\%~n2.pdb\%temp_file%\
+set temp_dir=temp
+set temp_file=!temp_dir!\%2
+set merge_exclusion=\%2\
+set delete_exclusion=\%2.config\!temp_dir!\
 
-if not exist %out_dir% (
+if not exist !out_dir! (
 	echo "Out directory %out_dir% not found."
 	exit /B 1
 )
-cd %out_dir%
+cd !out_dir!
+if not exist !temp_dir! md !temp_dir!
 
 if not exist %2  (
-	echo "Target file %out_file% not found."
+	echo "Target file !out_file! not found."
 	exit /B 1
 )
-@copy /y %out_file% %temp_file% >nul
+rem @copy /y %out_file% %temp_file% >nul
 
-set merge_cmd=%temp_file%
-for %%i in (*.dll) do (
+set merge_cmd=!out_file!
+for %%i in (*.dll *.exe) do (
 
-	IF /i "!exclusion:\%%~nxi\=!" equ "%exclusion%" (
+	IF /i "!merge_exclusion:\%%~nxi\=!" equ "!merge_exclusion!" (
 		set merge_cmd=!merge_cmd! %%i
 	)
 )
 
-set merge_cmd=%ilmerge_bin% /target:winexe /ndebug /out:%out_file% %merge_cmd%
+set merge_cmd=%ilmerge_bin% /target:winexe /out:!temp_file! !merge_cmd!
 
-echo %merge_cmd%
+echo !merge_cmd!
 %merge_cmd%
 
-set exclusion=!exclusion!!out_file!\
-echo %exclusion%
-for %%i in (*) do (
+rem set exclusion=!exclusion!!out_file!\
+rem echo %exclusion%
+rem for %%i in (*) do (
 
-	IF /i "!exclusion:\%%~nxi\=!" equ "%exclusion%" (
-		del %%i
-	)
-)
-del %temp_file%
+rem 	IF /i "!exclusion:\%%~nxi\=!" equ "%exclusion%" (
+rem 		del %%i
+rem 	)
+rem )
+rem del %temp_file%
