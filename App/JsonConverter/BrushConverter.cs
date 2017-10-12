093fcc26704f15b0f4107924bb800b1d5b88e14e
Nicholas Souphandavong <sonicolas@gmail.com>
2017-10-12 15:38:20 +0900

    Simplify.

--- App/JsonConverter/BrushConverter.cs
+++ App/JsonConverter/BrushConverter.cs
@@ -1,10 +1,8 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
-using System.IO;
 using System.Windows;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 
 namespace Taction.JsonConverter {
 
